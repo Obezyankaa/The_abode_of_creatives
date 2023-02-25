2a68751639d78218685c2d37ebdd511dc9d81d0d
Vladislav <voloshanowky@yandex.ru>
2023-02-25 16:13:39 +0300

    add dropdown meny decktop v5.6

--- src/App.tsx
+++ src/App.tsx
@@ -5,19 +5,19 @@ import { Layout } from './shared/Layout';
 import { Header } from './shared/Header/Header'
 import { Content } from './shared/Content';
 import { CardsList } from './shared/CardsList';
-import { Dropdown } from './shared/Dropdown';
+// import { Dropdown } from './shared/Dropdown';
 function AppComponent() {
   return (
     <Layout>
       <Header />
       <Content>
         <CardsList />
-        <div style={{padding: 20}}>
+        {/* <div style={{padding: 20}}>
           <br />
           <Dropdown onClose={() => console.log('closed')} onOpen={() => console.log('opened')} button={<button>Test</button>}>
             <div>block</div>
           </Dropdown>
-        </div>
+        </div> */}
       </Content>
     </Layout>
   )
--- src/shared/CardsList/Card/CardMenu/CardMenu.tsx
+++ src/shared/CardsList/Card/CardMenu/CardMenu.tsx
@@ -1,17 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { CardDropdownMenu } from '../CardDropdownMenu';
 import styles from './cardmenu.css';
 
+interface IDropsownProps {
+  open?: boolean;
+  setOpen?: () => void;
+}
+
 export function CardMenu() {
+    const [open, setOpen] = useState(false)
+  console.log('open===>', open);
+  
   return (
+    <>
  <div className={styles.menu}>
-        <button className={styles.menuButton}>
+      <button className={styles.menuButton} onClick={() => setOpen(!open)}>
             <svg width="5" height="20" viewBox="0 0 5 20" fill="none" xmlns="http://www.w3.org/2000/svg">
 <circle cx="2.5" cy="2.5" r="2.5" fill="#D9D9D9"/>
 <circle cx="2.5" cy="10" r="2.5" fill="#D9D9D9"/>
 <circle cx="2.5" cy="17.5" r="2.5" fill="#D9D9D9"/>
-</svg>
-
+            </svg>
         </button>
       </div>
+      {open ? (<CardDropdownMenu />) : (console.log('закрыт'))}
+    </>
   );
 }
