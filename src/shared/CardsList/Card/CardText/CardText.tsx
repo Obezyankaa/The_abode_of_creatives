10ff9ad959219d9ce5ae7fda3244732e9b280997
VladAir <vladair@MacBook-Air-VladAir.local>
2023-03-03 16:51:56 +0300

    редактировние дропменю

--- src/shared/CardsList/Card/CardText/CardText.tsx
+++ src/shared/CardsList/Card/CardText/CardText.tsx
@@ -8,11 +8,11 @@ export function CardText() {
           <div className={styles.userLink}>
             <img className={styles.avatar} src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlcnxlbnwwfHwwfHw%3D&w=1000&q=80" alt="фото профиля" />
             <a href="/autor" className={styles.username}>vlad voloshanowskiy</a>
-          </div>
           <span className={styles.createdAt}>
             <span className={styles.publishedLabel}>опубликовано</span>
             4 часа назад
           </span>
+          </div>
         </div>
         <h2 className={styles.title}>
           <a href="/post/autora" className={styles.postLink}>
